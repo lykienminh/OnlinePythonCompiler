1cea8956e2a1a78286a22ff20200868c99fda6da
Ly Kien Minh <lykienminh9@gmail.com>
2022-01-14 20:56:48 +0700

    final 31

--- OnlinePythonCompiler/wsgi.py
+++ OnlinePythonCompiler/wsgi.py
@@ -13,4 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OnlinePythonCompiler.settings')
 
-application = get_wsgi_application()
+# application = get_wsgi_application()
+
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
